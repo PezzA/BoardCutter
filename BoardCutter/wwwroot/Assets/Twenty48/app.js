6b0596134ac1fd002a1b7100597080c53b8e195f
PezzA <john.d.perrin@gmail.com>
2024-09-16 22:57:08 +0100

    First draft touch support (#6)

--- BoardCutter/wwwroot/Assets/Twenty48/app.js
+++ BoardCutter/wwwroot/Assets/Twenty48/app.js
@@ -42,6 +42,14 @@ function addCell(id, value, x, y) {
 window.initGrid = (dotNetHelper, cells) => {
     console.log('JS Game Init');
 
+    const gameArea = document.getElementById("gameArea");
+
+    window.dotNetHelper = dotNetHelper;
+
+    // https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+    gameArea.addEventListener('touchstart', handleTouchStart, false);
+    gameArea.addEventListener('touchmove', handleTouchMove, false);
+
     window.addEventListener("keydown", function (e) {
         if (e.repeat) return;
 
@@ -58,6 +66,51 @@ window.initGrid = (dotNetHelper, cells) => {
     }
     
     drawCells(cells);
+};
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    evt.preventDefault();
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    evt.preventDefault();
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            window.dotNetHelper.invokeMethodAsync('Move', 'ArrowLeft');
+        } else {
+            window.dotNetHelper.invokeMethodAsync('Move', 'ArrowRight');
+        }
+    } else {
+        if (yDiff > 0) {
+            window.dotNetHelper.invokeMethodAsync('Move', 'ArrowUp');
+        } else {
+            window.dotNetHelper.invokeMethodAsync('Move', 'ArrowDown');
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
 };
 
 function drawCells(cells){
