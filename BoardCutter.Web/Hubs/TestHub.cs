5ea67d663f18cdd6175cec03b9eca3b32790b597
PezzA <john.d.perrin@gmail.com>
2024-10-06 00:23:52 +0100

    Umbraco (#9)
    * first umbraco commit
    
    * settings
    
    * removed web .sln

---
+++ BoardCutter.Web/Hubs/TestHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Umbraco.Cms.Core.Security;
+
+public class TestHub : Hub<ITestHubEvents>
+{
+    private readonly IMemberManager _memberManager;
+
+    public TestHub(IMemberManager memberManager)
+    {
+        _memberManager = memberManager;
+    }
+    // when a client sends us a ping
+    public async Task Ping()
+    {
+
+        // we trigger the pong event on all clients
+        await Clients.Caller.Pong(message: Context?.User?.Identity?.Name ?? "na");
+    }
+}
