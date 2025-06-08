0a472de0a06d0c348b9ddd174dd21134d5b59775
PezzA <john.d.perrin@gmail.com>
2025-06-08 01:17:37 +0100

    added auth name to chat

--- BoardCutter.Web/Hubs/ChatHub.cs
+++ BoardCutter.Web/Hubs/ChatHub.cs
@@ -8,6 +8,6 @@ public class ChatHub : Hub
 {
     public async Task SendMessage(string message)
     {   
-        await Clients.All.SendAsync("ReceiveMessage", message);
+        await Clients.All.SendAsync("ReceiveMessage", Context.User?.Identity?.Name, message);
     }
 }
